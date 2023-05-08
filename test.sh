6341ab90c7af19b0ddbfed94c37de66c4fe124be
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-05-08 11:41:46 -0400

    cleanup

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PROJ_NAME = simsommodel
-source $$(conda info --base)/etc/profile.d/conda.sh ; conda activate $(PROJ_NAME)
-CONDA_ACTIVATE
-python3
-import simsommodel
-status=$?
-[ $(status) -eq 0 ] && echo "Successfully built SimSom environment" || echo "Building environment failed"
-exit()
