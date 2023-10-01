d01584dfaa1c30babb2ae3fc4b1827e035bd412f
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-01 18:30:29 -0400

    phigamma, thetagamma, thetaphi (6runs, multinodes)

---
+++ workflow/sbatch/thetagamma_multinodes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=28
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=22
+#SBATCH --time=3-23:59:00
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=thetagamma_multinodes_multinodes
+
+######  Module commands #####
+source /N/u/baotruon/Carbonate/mambaforge/etc/profile.d/conda.sh
+# conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/Carbonate/simsom
+echo '###### running thetagamma_multinodes exps ######'
+snakemake --nolock --snakefile workflow/rules/thetagamma_multinodes.smk --cores 22
