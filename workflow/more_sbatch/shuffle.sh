4a2b9bec5f5dfd6fbf6d15ea5ee8fb78709e3294
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-02 13:09:24 -0400

    shuffle 1 run

---
+++ workflow/more_sbatch/shuffle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=22
+#SBATCH --time=3-23:59:00
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=shuffle
+
+######  Module commands #####
+source /N/u/baotruon/Carbonate/mambaforge/etc/profile.d/conda.sh
+# conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/Carbonate/simsom
+echo '###### running shuffle exps ######'
+snakemake --nolock --snakefile workflow/more_rules/shuffle.smk --cores 22
