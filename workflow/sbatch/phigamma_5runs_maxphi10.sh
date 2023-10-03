c25961d7afa73e5b7d536eae47ab1fb53b55612b
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-03 14:15:23 -0400

    break up long exps phigamma, thetaphi into 2 runs

---
+++ workflow/sbatch/phigamma_5runs_maxphi10.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=36
+#SBATCH --time=1-23:59:00
+#SBATCH --mem=58gb
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=phigamma_mphi10
+
+######  Module commands #####
+source /N/u/baotruon/BigRed200/mambaforge/etc/profile.d/conda.sh
+# conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/BigRed200/simsom
+echo '###### running phigamma_mphi10 exps ######'
+snakemake --nolock --snakefile workflow/rules/phigamma_5runs_maxphi10.smk --cores 36
