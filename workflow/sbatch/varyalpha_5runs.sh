ecba6c7100b6a69ddd717e8defc9436e803bc7ca
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-03 14:24:53 -0400

    alpha exps

---
+++ workflow/sbatch/varyalpha_5runs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=36
+#SBATCH --time=1-23:59:00
+#SBATCH --mem=58gb
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=varyalpha_5runs
+
+######  Module commands #####
+source /N/u/baotruon/BigRed200/mambaforge/etc/profile.d/conda.sh
+# conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/BigRed200/simsom
+echo '###### running varyalpha_5runs exps ######'
+snakemake --nolock --snakefile workflow/rules/varyalpha_5runs.smk --cores 36
