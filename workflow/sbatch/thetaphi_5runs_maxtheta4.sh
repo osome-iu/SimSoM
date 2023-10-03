d83b075aaf48f0de9005e7fef47a9912f007b02f
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-03 16:19:54 -0400

    smk rule path

--- workflow/sbatch/thetaphi_5runs_maxtheta4.sh
+++ workflow/sbatch/thetaphi_5runs_maxtheta4.sh
@@ -18,4 +18,4 @@ source /N/u/baotruon/BigRed200/mambaforge/etc/profile.d/conda.sh
 ######  Job commands go below this line #####
 cd /N/u/baotruon/BigRed200/simsom
 echo '###### running thetaphi_mtheta4 exps ######'
-snakemake --nolock --snakefile workflow/rules/thetaphi_5runs_mtheta4.smk --cores 36
+snakemake --nolock --snakefile workflow/rules/thetaphi_5runs_maxtheta4.smk --cores 36
