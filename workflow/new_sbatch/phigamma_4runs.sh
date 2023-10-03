1d78e288d05cdd5277ad7ed5514f5d6ff7baf473
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-03 13:31:30 -0400

    bigred new rules

---
+++ workflow/new_sbatch/phigamma_4runs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=36
+#SBATCH --time=1-23:59:00
+#SBATCH --mem=58gb
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=phigamma_4runs
+
+######  Module commands #####
+source /N/u/baotruon/Carbonate/mambaforge/etc/profile.d/conda.sh
+# conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/Carbonate/simsom
+echo '###### running phigamma_4runs exps ######'
+snakemake --nolock --snakefile workflow/new_rules/phigamma_4runs.smk --cores 36
---
+++ workflow/new_sbatch/thetagamma_4runs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=36
+#SBATCH --time=1-23:59:00
+#SBATCH --mem=58gb
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=thetagamma_4runs
+
+######  Module commands #####
+source /N/u/baotruon/Carbonate/mambaforge/etc/profile.d/conda.sh
+# conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/Carbonate/simsom
+echo '###### running thetagamma_4runs exps ######'
+snakemake --nolock --snakefile workflow/new_rules/thetagamma_4runs.smk --cores 36
