4a0fa0f26e19ee4c94d16aa5525dd7e7adc8de34
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-12 15:44:37 -0400

    v3.2 alpha mu exps on network_baseline.gml

---
+++ workflow/sbatch/varyalpha_nobots.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=57
+#SBATCH --time=3-23:59:00
+#SBATCH --mem=58gb
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=varyalpha_nobots
+
+######  Module commands #####
+source /N/u/baotruon/BigRed200/conda/etc/profile.d/conda.sh
+conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/BigRed200/simsom
+echo '###### running varyalpha_nobots exps ######'
+snakemake --rerun-incomplete --nolock --snakefile workflow/rules/varyalpha_nobots.smk --cores 57
