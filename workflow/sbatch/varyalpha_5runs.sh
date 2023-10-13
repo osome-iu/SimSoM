d9753c8863ca338548bd76034a204c02f969a287
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-12 21:46:17 -0400

    v3.2 exps wall time & cores

--- workflow/sbatch/varyalpha_5runs.sh
+++ workflow/sbatch/varyalpha_5runs.sh
@@ -4,8 +4,8 @@
 #SBATCH --mail-user=baotruon@iu.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=43
-#SBATCH --time=1-23:59:00
+#SBATCH --cpus-per-task=57
+#SBATCH --time=3-23:59:00
 #SBATCH --mem=58gb
 #SBATCH --mail-type=FAIL,BEGIN,END
 #SBATCH --job-name=varyalpha_5runs
@@ -18,4 +18,4 @@ conda activate simsommodel
 ######  Job commands go below this line #####
 cd /N/u/baotruon/BigRed200/simsom
 echo '###### running varyalpha_5runs exps ######'
-snakemake --nolock --snakefile workflow/rules/varyalpha_5runs.smk --cores 43
+snakemake --nolock --snakefile workflow/rules/varyalpha_5runs.smk --cores 57
