4e18b5a051202bb337c69ce1a3c0253ea6c290b1
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-17 16:32:10 -0400

    v2 exps bigred

---
+++ experiments/10172023_v2_exps/sbatch/thetaphi_5runs_maxtheta4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=57
+#SBATCH --time=3-23:59:00
+#SBATCH --mem=58gb
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=sm_thetaphi_mtheta4
+
+######  Module commands #####
+source /N/u/baotruon/BigRed200/conda/etc/profile.d/conda.sh
+conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/BigRed200/simsom
+echo '###### running sm_thetaphi_mtheta4 exps ######'
+snakemake --nolock --snakefile experiments/10172023_v2_exps/rules/thetaphi_5runs_maxtheta4.smk --cores 57
