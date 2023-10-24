cc6d882b421ca65e02b94b59d684c43fd7f5d193
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-24 15:37:53 -0400

    test script save all verbose results

--- experiments/10142023_v3.3_exps/run_simulations.py
+++ experiments/10142023_v3.3_exps/run_simulations.py
@@ -36,24 +36,28 @@ network_fpath = os.path.join(DATA_PATH, "infosys_network.gml")
 # network_fpath = os.path.join(DATA_PATH, "network_baseline.gml")
 ## SPECS
 alpha = 15
+phi = 0.1
 simulation_specs = {
     "verbose": False,
     "tracktimestep": True,
-    "save_message_info": False,
+    "save_message_info": True,
     "output_cascades": False,
-    "epsilon": 0.001,
+    "epsilon": 0.0001,
     "mu": 0.5,
     "alpha": alpha,
+    "phi": phi,
     "n_threads": 12,
 }
-no_runs = 1
+no_runs = 5
 
 ## OLD MODEL
 
-RESULT_DIR = "experiments/10142023_v3.3_exps/results_local"
-reshare_fpath = os.path.join(RESULT_DIR, f"reshare_alpha{alpha}.csv")
-exposure_fpath = os.path.join(RESULT_DIR, f"exposure_alpha{alpha}.csv")
-message_info_fpath = os.path.join(RESULT_DIR, f"message_info_alpha{alpha}.json.gz")
+RESULT_DIR = "experiments/10142023_v3.3_exps/reshare_by_bot_status"
+reshare_fpath = os.path.join(RESULT_DIR, f"reshare_alpha{alpha}_phi{phi}.csv")
+exposure_fpath = os.path.join(RESULT_DIR, f"exposure_alpha{alpha}_phi{phi}.csv")
+message_info_fpath = os.path.join(
+    RESULT_DIR, f"message_info_alpha{alpha}_phi{phi}.json.gz"
+)
 
 if not os.path.exists(RESULT_DIR):
     os.makedirs(RESULT_DIR)
@@ -63,29 +67,26 @@ if not os.path.exists(RESULT_DIR):
 # Create a list to store results across runs
 # quality = []
 
-metrics = ["quality", "diversity", "discriminative_pow", "age_timestep"]
+metrics = [
+    "quality",
+    "diversity",
+    "discriminative_pow",
+    #    "age_timestep"
+]
 res_list = defaultdict(list)
 
 print("*** Start simulation ***")
 for run in range(no_runs):
     print(f"-- Run {run+1}/{no_runs}: \n Create SimSom instance..")
-    if run == 0:
-        simulation_specs_ = deepcopy(simulation_specs)
-        simulation_specs_["save_message_info"] = True
-        follower_sys = SimSom(network_fpath, **simulation_specs_)
-        print(follower_sys)
-
-    else:
-        # Create a SimSom instance
-        follower_sys = SimSom(network_fpath, **simulation_specs)
+    # Create a SimSom instance
+    follower_sys = SimSom(network_fpath, **simulation_specs)
 
     # Run simulation
     if simulation_specs["output_cascades"] is False:
         results = follower_sys.simulation()
     else:
         results = follower_sys.simulation(
-            reshare_fpath=reshare_fpath.replace(".csv", f"_{run}.csv"),
-            exposure_fpath=exposure_fpath.replace(".csv", f"_{run}.csv"),
+            reshare_fpath=reshare_fpath.replace(".csv", f"_{run}.csv")
         )
     print(f" - Simulation finished. Quality: {np.round(results['quality'],3)}")
 
@@ -95,20 +96,22 @@ for run in range(no_runs):
         res_list[metric] += [results[metric]]
 
     # Save verbose results (with simulation specs)
-    if run == 0:
-        # if simulation_specs["save_message_info"] is True:
-        specs = deepcopy(simulation_specs)
-        specs.update(results)
-        fpath = message_info_fpath.replace(".json.gz", f"_{run}.json.gz")
-        write_json_compressed(fpath, specs)
+
+    # if simulation_specs["save_message_info"] is True:
+    specs = deepcopy(simulation_specs)
+    specs.update(results)
+    fpath = message_info_fpath.replace(".json.gz", f"_{run}.json.gz")
+    write_json_compressed(fpath, specs)
+
+print(
+    f"*** Average quality across {no_runs} runs: {np.round(np.mean(res_list['quality']),3)} ***"
+)
+
 
 # Save short results (with simulation specs)
 short_results = deepcopy(simulation_specs)
 short_results.update(res_list)
 json.dump(
-    short_results, open(os.path.join(RESULT_DIR, f"results_alpha{alpha}.json"), "w")
-)
-
-print(
-    f"*** Average quality across {no_runs} runs: {np.round(np.mean(res_list['quality']),3)} ***"
+    short_results,
+    open(os.path.join(RESULT_DIR, f"results_alpha{alpha}_phi{phi}.json"), "w"),
 )
