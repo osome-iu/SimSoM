bdd962200852de89d943e90c831bf21c1122579a
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-10-30 22:53:17 -0400

    v3.3 exps

---
+++ workflow/sbatch_33/theta.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=57
+#SBATCH --time=3-23:59:00
+#SBATCH --mem=58gb
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=f33_theta
+
+######  Module commands #####
+source /N/u/baotruon/BigRed200/conda/etc/profile.d/conda.sh
+conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/BigRed200/simsom
+echo '###### running f33_theta exps ######'
+snakemake --nolock --rerun-triggers mtime --rerun-incomplete --snakefile workflow/rules_33/theta.smk --cores 57
