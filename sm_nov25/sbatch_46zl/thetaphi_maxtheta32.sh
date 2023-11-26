aa11e48c6620ba04c589379ea84ee85cce5b7a04
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-11-25 23:20:03 -0800

    exps quality exp=10 small net (appeal 2, 5)

---
+++ sm_nov25/sbatch_46zl/thetaphi_maxtheta32.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=21
+#SBATCH --time=3-23:59:00
+#SBATCH --mem=58gb
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=thetaphi_t32_sm46zl
+
+######  Module commands #####
+source /N/u/baotruon/BigRed200/conda/etc/profile.d/conda.sh
+conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/BigRed200/simsom
+echo '###### running thetaphi_t32_sm46zl exps ######'
+snakemake --nolock --rerun-triggers mtime --rerun-incomplete --snakefile sm_nov25/rules_46zl/thetaphi_maxtheta32.smk --cores 21
