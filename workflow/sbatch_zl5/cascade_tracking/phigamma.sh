9fc83918b57c542e2ddf2d2eaab6ff4274d30435
btrantruong <27907677+btrantruong@users.noreply.github.com>
2023-11-30 16:26:06 -0800

    cascade tracking exps

---
+++ workflow/sbatch_zl5/cascade_tracking/phigamma.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH -A r00382
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=57
+#SBATCH --time=3-23:59:00
+#SBATCH --mem=58gb
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=fzl5_phigamma_mphi4_cascade_tracking
+
+######  Module commands #####
+source /N/u/baotruon/BigRed200/conda/etc/profile.d/conda.sh
+conda activate simsommodel
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/BigRed200/simsom
+echo '###### running fzl5_phigamma_mphi4_cascade_tracking exps ######'
+snakemake --rerun-incomplete --nolock --rerun-triggers mtime --snakefile workflow/rules_zl5/cascade_tracking/phigamma.smk --cores 57
